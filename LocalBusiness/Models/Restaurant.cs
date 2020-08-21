abc3adfda0615a841aabf4afb4c657c240c94376
Taylor Somers <tsomers@yandex.com>
2020-08-21 16:53:03 -0400

    Add Restaurant class and properties

---
+++ LocalBusiness/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+namespace LocalBusiness.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+
+    public string RestaurantName { get; set; }
+
+    public string RestaurantAddress { get; set; }
+
+    public string RestaurantType { get; set; }
+
+    public string RestaurantYelpRating { get; set; }
+  }
+}
